dc346ac602f1cc7e53b347a0ea3dae6465971714
IsaiasCabreraG <isaiasgoku15@gmail.com>
2025-11-06 21:42:41 -0300

    endpoint crear alertas

--- backend/alertas/views.py
+++ backend/alertas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from proyectos.models import Proyecto, Actividad, ActividadDifusion
+from proyectos.models import *
 from django.db.models import Prefetch
 from django.http import JsonResponse
 import json
+from datetime import datetime
+from django.utils import timezone
 
 
 # Create your views here.
@@ -63,10 +65,46 @@ def modificar_alerta(request):
         return JsonResponse({"status": "error", "message": "Invalid request method"}, status=400)
 
 def crear_alertas(request):
-    print("Crear alertas")
-    data = json.loads(request.body)
-    print("Datos recibidos:", data)
-    return JsonResponse({"status": "success"})
+    if request.method == "POST":
+        try:
+            datos = json.loads(request.body)
+            print("Datos recibidos:", datos)
+            
+            if isinstance(datos, dict):
+                 datos = [datos]
+                 
+            ahora = timezone.now()
+            
+            # --- Proceso de Alertas ---
+            for item in datos:
+                print("creando alerta para: ", item)
+                actividad_id = item.get("actividad")
+                fecha_envio_str = item.get("fecha")
+                
+
+
+                fecha_envio_dt = datetime.strptime(fecha_envio_str, "%Y-%m-%d %H:%M:%S").replace(microsecond=0)
+                    
+                fecha_envio_aware = timezone.make_aware(fecha_envio_dt)
+
+                if fecha_envio_aware > ahora:
+                    actividad = get_object_or_404(ActividadBase, id=actividad_id)
+                    
+                    Alerta.objects.create(
+                        actividad=actividad, 
+                        fecha_envio=fecha_envio_aware,
+                        enviado=False, 
+                    )
+                    print(f"Alerta creada para Actividad ID {actividad_id}")
+                else:
+                    print(f"La fecha: {fecha_envio_aware} ya pasó, no se creo la alerta")
+                    
+            return JsonResponse({"success": True, "mensaje": "Alertas creadas correctamente"})
+        
+        except Exception as e:
+            # Captura y reporta errores como Actividad no encontrada, formato de fecha, etc.
+            print(f"ERROR DURANTE LA CREACIÓN DE ALERTA: {type(e).__name__}: {str(e)}")
+            return JsonResponse({"success": False, "mensaje": f"Error: {str(e)}"}, status=400)
 
 def mover_alertas(request):
     print("Modificar alertas")
